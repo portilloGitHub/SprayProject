c74f0b8f233e16f127e3185b5449998a0f124e4a
Alberto Portillo <56606601+portilloGitHub@users.noreply.github.com>
2021-09-02 15:52:36 -0500

    Code review and minor changes

--- SprayProject/Implement.cpp
+++ SprayProject/Implement.cpp
@@ -11,68 +11,66 @@ namespace trimble
 {
 
 
-//=============================================================================
-//=============================================================================
-//                             Implementation
-//=============================================================================
-//=============================================================================
+	//=============================================================================
+	//=============================================================================
+	//                             Implementation
+	//=============================================================================
+	//=============================================================================
 
-//-----------------------------------------------------------------------------
-// CImplement Constructor
-//-----------------------------------------------------------------------------
-CImplement::CImplement(double width, double distanceToTractor):
-  _Width(width),
-  _DistanceToTractor(distanceToTractor),
-  _currHeading(0.0),
-  _isSpraying(false)
-{
-}
-
-
-//-----------------------------------------------------------------------------
-// CImplement::HandleNewTractorPosition(...)
-//-----------------------------------------------------------------------------
-void CImplement::HandleNewTractorPosition(const CEnuPosition& tractorPos,
-                                          double heading)
-{
-  //...
-  UpdateAppliedArea(GenerateImplementPos(tractorPos, heading), heading);
-  //...
-}
+	//-----------------------------------------------------------------------------
+	// CImplement Constructor
+	//-----------------------------------------------------------------------------
+	CImplement::CImplement(double width, double distanceToTractor) :
+		_Width(width),
+		_DistanceToTractor(distanceToTractor),
+		_currHeading(0.0),
+		_isSpraying(false)
+	{
+	}
 
 
-//-----------------------------------------------------------------------------
-// CImplement::UpdateAppliedArea(...)
-//-----------------------------------------------------------------------------
-void CImplement::UpdateAppliedArea(const CEnuPosition& newImplementPos,
-                                   double heading)
-{
-  CEnuPosition newLPos = LeftPos(m_width/2);
-  CEnuPosition newRtPos = RightPos(m_width/2);
-  CPolygon newPoly = GeneratePolygon(_currLPos,
-                                     _currRPos,
-                                     newLPos,
-                                     newRPos);
-  
-  // Turn the nozzles off if the spray polygon overlaps where we've already
-  // sprayed
-  bool turnNozzlesOn = !_appliedArea.CheckOverlap(newPoly);
-
-  if (_isSpraying)
-  {
-    // The spray we've got overlaps - turn off the nozzles
-    _appliedArea.AddPolygon(newPoly);
-  }
-
-  SetAllNozzles(turnNozzlesOn);
-  
-  _isSpraying = turnNozzlesOn;
-  _currPos = newImplementPos;
-  _currHeading = heading;
-  _currRPos = newRPos;
-  _currLPos = newLPos;
-} 
+	//-----------------------------------------------------------------------------
+	// CImplement::HandleNewTractorPosition(...)
+	//-----------------------------------------------------------------------------
+	void CImplement::HandleNewTractorPosition(const CEnuPosition& tractorPos,
+		double heading)
+	{
+		//...
+		UpdateAppliedArea(GenerateImplementPos(tractorPos, heading), heading);
+		//...
+	}
 
 
+	//-----------------------------------------------------------------------------
+	// CImplement::UpdateAppliedArea(...)
+	//-----------------------------------------------------------------------------
+	void CImplement::UpdateAppliedArea(const CEnuPosition& newImplementPos,
+		double heading)
+	{
+		CEnuPosition newLPos = LeftPos(m_width / 2);
+		CEnuPosition newRtPos = RightPos(m_width / 2);
+		CPolygon newPoly = GeneratePolygon(_currLPos,
+			_currRPos,
+			newLPos,
+			newRPos);
+
+		// Turn the nozzles off if the spray polygon overlaps where we've already
+		// sprayed
+		bool turnNozzlesOn = !_appliedArea.CheckOverlap(newPoly);
+
+		if (_isSpraying)
+		{
+			// The spray we've got overlaps - turn off the nozzles
+			_appliedArea.AddPolygon(newPoly);
+		}
+
+		SetAllNozzles(turnNozzlesOn);
+
+		_isSpraying = turnNozzlesOn;
+		_currPos = newImplementPos;
+		_currHeading = heading;
+		_currRPos = newRPos;
+		_currLPos = newLPos;
+	}
 }
 
--- SprayProject/Implement.hpp
+++ SprayProject/Implement.hpp
@@ -16,64 +16,64 @@ namespace trimble
 {
 
 
-//-----------------------------------------------------------------------------
-// Class
-//-----------------------------------------------------------------------------
-public CImplement : public ITractorPositionListener
-{
-public:
-  CImplement(double width, double distanceToTractor);
+	//-----------------------------------------------------------------------------
+	// Class
+	//-----------------------------------------------------------------------------
+	public CImplement : public ITractorPositionListener
+	{
+	public:
+	  CImplement(double width, double distanceToTractor);
 
-  // Retrieve configured width and distance to tractor
-  double Width() const { return _Width; }
-  double DistanceToTractor() const { return _DistanceToTractor; }
+	  // Retrieve configured width and distance to tractor
+	  double Width() const { return _Width; }
+	  double DistanceToTractor() const { return _DistanceToTractor; }
 
-  // Retrieves the current position of the implement (center)
-  CEnuPosition Position() const { return _currPos; }
+	  // Retrieves the current position of the implement (center)
+	  CEnuPosition Position() const { return _currPos; }
 
-  // Retrieve the area where spray has been applied
-  const CAppliedArea& AppliedArea() const { return _appliedArea; }
+	  // Retrieve the area where spray has been applied
+	  const CAppliedArea& AppliedArea() const { return _appliedArea; }
 
-  // Indicates whether or not the implement is currently spraying
-  bool IsSpraying() const { return _isSpraying; }
+	  // Indicates whether or not the implement is currently spraying
+	  bool IsSpraying() const { return _isSpraying; }
 
-  // ITractorPositionListener method
-  virtual void HandleNewTractorPosition(const CEnuPosition& tractorPos,
-                                        double heading);
+	  // ITractorPositionListener method
+	  virtual void HandleNewTractorPosition(const CEnuPosition& tractorPos,
+											double heading);
 
-protected:
-  // Generates the center position of the implement given the tractor pos
-  CEnuPosition GenerateImplementPos(const CEnuPosition& tractorPos,
-                                    double heading) const;
+	protected:
+		// Generates the center position of the implement given the tractor pos
+		CEnuPosition GenerateImplementPos(const CEnuPosition& tractorPos,
+										  double heading) const;
 
-  // Gets a position to the left of the implement center
-  CEnuPosition LeftPos(double distanceFromCenter) const { return _currLPos; }
+		// Gets a position to the left of the implement center
+		CEnuPosition LeftPos(double distanceFromCenter) const { return _currLPos; }
 
-  // Gets a position to the right of the implement center
-  CEnuPosition RightPos(double distanceFromCenter) const { return _currRPos; }
+		// Gets a position to the right of the implement center
+		CEnuPosition RightPos(double distanceFromCenter) const { return _currRPos; }
 
-  // Generates a polygon given the where the implement ends were and are now
-  CPolygon GeneratePolygon(const CEnuPosition& backLeft,
-                           const CEnuPosition& backRight,
-                           const CEnuPosition& frontLeft,
-                           const CEnuPosition& frontRight) const;
+		// Generates a polygon given the where the implement ends were and are now
+		CPolygon GeneratePolygon(const CEnuPosition& backLeft,
+								 const CEnuPosition& backRight,
+								 const CEnuPosition& frontLeft,
+								 const CEnuPosition& frontRight) const;
 
-  // Turns all the nozzles on or off so they activly start or stop spraying
-  void SetAllNozzles(bool on);
+		// Turns all the nozzles on or off so they activly start or stop spraying
+		void SetAllNozzles(bool on);
 
-  // Updates the applied area given a new implement position and heading
-  void UpdateAppliedArea(const CEnuPosition& newImplementPos, double heading);
+		// Updates the applied area given a new implement position and heading
+		void UpdateAppliedArea(const CEnuPosition& newImplementPos, double heading);
 
-private:
-  const double  _Width;
-  const double  _DistanceToTractor;
-  double        _currHeading;
-  CENUPosition  _currPos;
-  CENUPosition  _currLPos;
-  CENUPosition  _currRPos;
-  CAppliedArea  _appliedArea;
-  bool          _isSpraying;
-};
+	  private:
+		const double  _Width;
+		const double  _DistanceToTractor;
+		double        _currHeading;
+		bool          _isSpraying;
+		CENUPosition  _currPos;
+		CENUPosition  _currLPos;
+		CENUPosition  _currRPos;
+		CAppliedArea  _appliedArea;
+	};
 
 
 }
